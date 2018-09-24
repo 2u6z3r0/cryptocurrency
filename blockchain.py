da88fedd39aaca0fc55a34deec84c6011ced8e85
2u6z3r0 <samrat.patil0202@gmail.com>
2018-09-24 16:10:12 +1000

    Now using dict type for transactino as its more realastic. added documentation. made changes to add_transaction method. New function to mine a block(block_mine). And other changes

--- blockchain.py
+++ blockchain.py
@@ -1,18 +1,56 @@
 # simple simulator of blcokchain using lists
 
-blockchain = []
+genesis_block = {
+    'previous_block_hash':'',
+    'index':0,
+    'transaction': []
+}
+blockchain = [genesis_block]
+open_transactions=[]
+owner = 'Max'
 
 def get_last_blockchain_value():
-        if len(blockchain) < 1:
-            return [1]
-        return blockchain[-1]
+    """ Return last block value from the blockchain.
 
-def add_value(transanction_amount):
-    blockchain.append([get_last_blockchain_value(), transanction_amount])
-    print(blockchain)
-    print("transanction added")
+    Returns:
+        last block of the blockchain
+    """
+    if len(blockchain) < 1:
+        return [1]
+    return blockchain[-1]
+
+def add_transaction(receiver, sender=owner, amount=1.0):
+    """ Add a transaction to the open transaction list
+    
+    Args:
+        sender: sender of the coins
+        receiver: receiver of the coins
+        amount: the amount of coins sent with transaction(default=1.0)
+    """
+    transaction = {'sender':sender, 'receiver': receiver, 'amount':amount}
+    open_transactions.append(transaction)    
+
+def block_mine():
+    last_block = blockchain[-1]
+    hashed_last_block=''
+    for key in last_block:
+        hashed_last_block += str(last_block[key])
+    print(hashed_last_block)
+    
+    block = {
+        'previous_block_hash':hashed_last_block,
+        'index':len(blockchain),
+        'transaction': open_transactions
+    }
+    blockchain.append(block)
 
 def validate_blockchain():
+    """ Validates the blockchain.
+
+    Retuns:
+        True - if blockchain is valid
+        False - if blockchian is not valid
+    """
     block_index = 0
     is_valid = True
     for block in blockchain:
@@ -23,42 +61,62 @@ def validate_blockchain():
             is_valid = True
         else:
             is_valid = False
-            break
+            break 
         block_index += 1
     return is_valid
     
-def get_transanction_value():
-    return float(input("Enter your transanction amount: "))
+def get_transaction_value():
+    """ Get transaction information(recipient, amount) from the user.
+
+    Returns:
+        tuple(receiver, amount) 
+    """
+    tx_receiver = input("Enter recipient name: ")
+    tx_amount = float(input("Enter your transaction amount: "))
+    return (tx_receiver, tx_amount)
 
 def print_blockchain():
+    """ Prints entire blockchain.
+    """
     print("Printing blockchain")
     for block in blockchain:
         print(block)
     print("Done!")
 
 def get_user_choice():
-    return input('Your choice: ')
+    """ Takes user input for choice 
     
+    Retuns:
+        user choice
+    """
+
+    return input('Your choice: ')
+
 while True:
     print("\nPlease choose")
-    print("1. Add a transanction value")
-    print("2. Print blockchian")
-    print("3. Quit")
-    print("4. Hack the planet")
+    print("1. Add a transaction value")
+    print("2. Mine a block")
+    print("3. Print blockchian")
+    print("4. Quit")
+    print("5. Hack the planet")
     user_choice = get_user_choice()
     if user_choice == '1':
-        tr_amount = get_transanction_value()
-        add_value(tr_amount)
+        tx_data = get_transaction_value()
+        recipient, amount = tx_data
+        add_transaction(recipient, amount=amount)
+        print(open_transactions)
     elif user_choice == '2':
-        print_blockchain()
+        block_mine()
     elif user_choice == '3':
-        break
+        print_blockchain()
     elif user_choice == '4':
+        break
+    elif user_choice == '5':
         if len(blockchain) > 0:
             blockchain[0] = [2]
     else:
         print("Invalid choice")
-    if not validate_blockchain():
-        print('Invalid Chain!!!')
-        break
+    # if not validate_blockchain():
+    #     print('Invalid Chain!!!')
+    #     break
 
